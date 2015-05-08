31f18ca53981ea84eb26db3948ddc466568bf43c
MintyChipp <mintychipp@gmail.com>
2015-05-08 15:43:48 -0400

    Made it reverse, and renamed copycat reverse

--- reverse.cpp
+++ reverse.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
   char saystr[100];
-  cout << "Enter a string and i will output it for you: ";
+  cout << "Enter a string and i will output it for you backwards: ";
   cin.getline(saystr, 100);
   cout << "\n";
+  reverse(saystr, saystr + strlen(saystr));
   cout << saystr;
   cout << "\n";
   return 0;
