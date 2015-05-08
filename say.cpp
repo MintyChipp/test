759127f61e1906fd1eceb3cecfdc2a47a038f665
MintyChipp <mintychipp@gmail.com>
2015-05-08 00:05:41 -0400

    added prompt and newlines

--- say.cpp
+++ say.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
   char saystr[100];
-  cin >> saystr;
-  cout << saystr;
+  cout << "Enter a string and i will output it for you:%s\n"
+  cin >> saystr, "\n";
+  cout << saystr, "\n";
   return 0;
 }
